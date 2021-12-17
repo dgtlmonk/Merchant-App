21f98da71bb7b184b1a0f96cfe2084c02c484de3
Joel Pablo <dgtlmonk@gmail.com>
2021-12-17 17:30:01 +0800

    feat: add router (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
 import "@/styles/App.css";
-import { useState } from "react";
+import { CircularProgress } from "@material-ui/core";
+import { useEffect, useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
 import AddSales from "./components/AddSales";
 import AppMenu from "./components/AppMenu";
 import IssueCard from "./components/IssueCard";
 import LoginForm from "./components/LoginForm";
 import { VIEWS } from "./types";
 
+function useQuery() {
+  const { search } = useLocation();
+
+  return useMemo(() => new URLSearchParams(search), [search]);
+}
+
 function App() {
-  const [viewState, setViewState] = useState<string>(VIEWS.ADD_SALES);
+  const [viewState, setViewState] = useState<string>(VIEWS.IDLE);
+
+  const [module, setModule] = useState<any>(null);
+  const [token, setToken] = useState<any>(null);
+
+  const r = useQuery();
+
+  useEffect(() => {
+    if (r.getAll("module")) {
+      setModule(r.getAll("module")[0]);
+    }
+
+    if (r.getAll("token")) {
+      setToken(r.getAll("token")[0]);
+    }
+  }, [r]);
+
+  useEffect(() => {
+    if (module === "0") {
+      setViewState(VIEWS.MENU);
+      return;
+    }
+    if (module === "1") {
+      setViewState(VIEWS.ISSUE_CARD);
+      return;
+    }
+
+    if (module === "2") {
+      setViewState(VIEWS.ADD_SALES);
+      return;
+    }
+
+    setViewState(VIEWS.LOGIN);
+  }, [module]);
 
   const handleBackToMenu = () => {
     setViewState(VIEWS.MENU);
@@ -21,6 +62,11 @@ function App() {
     <div className="flex flex-col p-4 items-center h-full">
       {
         {
+          [VIEWS.IDLE]: (
+            <div className="flex justify-center items-center h-full">
+              <CircularProgress size="2rem" />
+            </div>
+          ),
           [VIEWS.LOGIN]: (
             <LoginForm
               onSuccess={() => {
