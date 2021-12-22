df6cec606674622379c9b6a4250e0c94183847f4
dgtlmonk <dgtlmonk@gmail.com>
2021-12-22 11:03:21 +0800

    styles: (wip) ui add sales

--- src/components/AddSales.tsx
+++ src/components/AddSales.tsx
@@ -1,6 +1,7 @@
 // import { client } from "@/helpers/api-client";
 
 import { CircularProgress, InputAdornment, TextField } from "@material-ui/core";
+import { ArrowBack } from "@material-ui/icons";
 import { useEffect, useRef, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { client } from "../helpers/api-client";
@@ -85,34 +86,7 @@ function Index({ onDone }: Props) {
       setIsSubmitting(false);
       setIsSearchingSuccess(false);
 
-      setTimeout(() => {
-        try {
-          // @ts-ignore
-          cardNumberRef?.current?.focus();
-
-          // @ts-ignore
-          cardNumberRef?.current?.focus();
-
-          // @ts-ignore
-          qttyRef.current.value = 0;
-          // @ts-ignore
-          cardNumberRef?.current?.focus();
-
-          // @ts-ignore
-          cardNumberRef?.current?.focus();
-
-          // @ts-ignore
-          cardNumberRef?.current?.focus();
-
-          // @ts-ignore
-          receiptRef.current.value = 0;
-          // @ts-ignore
-          cardNumberRef?.current?.focus();
-
-          // @ts-ignore
-          amountRef.current.value = 0;
-        } catch {}
-      }, 500);
+      resetRefs();
       return;
     }
 
@@ -159,96 +133,162 @@ function Index({ onDone }: Props) {
         }),
       })
       .then((res) => {
-        console.log(" add sales response ", res);
         setIsSubmitSuccess(true);
       })
       .finally(() => setIsSubmitting(false));
   };
 
+  function handleReset() {
+    setIsSubmitSuccess(false);
+    setIsSubmitting(false);
+    setIsSearchingSuccess(false);
+    resetRefs();
+  }
+
+  function resetRefs() {
+    setTimeout(() => {
+      try {
+        // @ts-ignore
+        cardNumberRef?.current?.focus();
+
+        // @ts-ignore
+        cardNumberRef?.current?.focus();
+
+        // @ts-ignore
+        qttyRef.current.value = 0;
+        // @ts-ignore
+        cardNumberRef?.current?.focus();
+
+        // @ts-ignore
+        cardNumberRef?.current?.focus();
+
+        // @ts-ignore
+        cardNumberRef?.current?.focus();
+
+        // @ts-ignore
+        receiptRef.current.value = 0;
+        // @ts-ignore
+        cardNumberRef?.current?.focus();
+
+        // @ts-ignore
+        amountRef.current.value = 0;
+      } catch {}
+    }, 500);
+  }
+
   return (
-    <div className="flex flex-col max-w-md items-center justify-center w-full h-full p-4">
+    // <div className="flex flex-col max-w-md items-center justify-center w-full h-full p-4">
+
+    <div className="flex flex-col w-full h-full items-center">
       <div
-        className={`flex w-full px-4 py-2 border justify-center  rounded-md bg-orange-400 text-white text-sm mb-4 
-        ${isSubmitSuccess ? "visible" : "hidden"}`}
+        className="module__header sticky top-0 w-full z-20 mb-2"
+        style={{ backgroundColor: "#f8f8ff" }}
       >
-        Sales added successfully
-      </div>
-
-      <div className="flex flex-col w-full ">
-        {isSearchingSuccess ? (
-          <div className="flex flex-row justify-between">
-            {person}
-            <button onClick={() => setIsSearchingSuccess(false)}>X</button>
-          </div>
-        ) : (
-          <TextField
-            label="Member Card Number"
-            className="w-full"
-            variant="filled"
-            disabled={isSearching}
-            inputRef={cardNumberRef}
-            defaultValue=""
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="start">
-                  <span className="p-4 px-2" onClick={handleSearchMemberCard}>
-                    {isSearching ? (
-                      <CircularProgress size="1rem" />
-                    ) : (
-                      <FaSearch className="opacity-40" />
-                    )}
-                  </span>
-                </InputAdornment>
-              ),
-            }}
-          />
-        )}
-        <div className="flex flex-row mt-8">
-          <span className="flex mr-2">
-            <TextField label="Receipt Number" inputRef={receiptRef} />
-          </span>
-
-          <span className="flex mr-2">
-            <TextField
-              inputRef={qttyRef}
-              label="Quantity"
-              type="number"
-              defaultValue="0"
-            />
-          </span>
-          <TextField
-            inputRef={amountRef}
-            label="Total Amount"
-            defaultValue="0.00"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">$</InputAdornment>
-              ),
-            }}
-          />
+        <div className="flex flex-row justify-center items-center relative w-full h-16">
+          <button className={`absolute top-0 left-0 h-16 w-16`}>
+            <ArrowBack className="opacity-50" />
+          </button>
+          <div className="p-4">Sales</div>
         </div>
       </div>
-      <div className="flex flex-row w-full items-center justify-center ">
-        {isSubmitting ? (
-          <div className="flex flex-row w-full items-center justify-center mt-8">
-            <CircularProgress size="1.5rem" />
+
+      <div className="flex max-w-md flex-col justify-center z-10 p-8">
+        <div
+          className={`flex w-full px-4 py-2 border justify-center  rounded-md bg-orange-400 text-white text-sm mb-4 
+        ${isSubmitSuccess ? "visible" : "hidden"}`}
+        >
+          Sales added successfully
+        </div>
+
+        <div className="flex flex-col w-full">
+          {isSearchingSuccess ? (
+            <div className="flex flex-row justify-between">
+              {person}
+              <button onClick={handleReset}>X</button>
+            </div>
+          ) : (
+            <TextField
+              label="Member Card Number"
+              className="w-full"
+              variant="filled"
+              disabled={isSearching}
+              inputRef={cardNumberRef}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleSearchMemberCard();
+                  e.preventDefault();
+                }
+              }}
+              defaultValue=""
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start">
+                    <span className="p-4 px-2" onClick={handleSearchMemberCard}>
+                      {isSearching ? (
+                        <CircularProgress size="1rem" />
+                      ) : (
+                        <FaSearch className="opacity-40" />
+                      )}
+                    </span>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          )}
+          <div className="flex flex-row mt-8">
+            <span className="flex mr-2 w-3/5 leading-tight">
+              <TextField
+                label="Receipt Number"
+                inputRef={receiptRef}
+                InputLabelProps={{ style: { fontSize: 15 } }} // font size of input text
+              />
+            </span>
+            <span className="flex mr-2 w-1/5">
+              <TextField
+                inputRef={qttyRef}
+                InputLabelProps={{ style: { fontSize: 15 } }} // font size of input text
+                label="Quantity"
+                type="number"
+                defaultValue="0"
+              />
+            </span>
+            <TextField
+              inputRef={amountRef}
+              InputLabelProps={{ style: { fontSize: 15 } }} // font size of input text
+              label="Total Amount"
+              defaultValue="0.00"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="end">
+                    <span className="mr-2 text-gray-400">$</span>
+                  </InputAdornment>
+                ),
+              }}
+            />
           </div>
-        ) : isSearchingSuccess ? (
-          <div className="flex flex-row w-full items-center justify-center mt-8">
-            <button
-              disabled={isSubmitting}
-              className="p-2 px-8 border rounded-md  bg-slate-400 text-white mr-4"
-            >
-              Cancel
-            </button>
-            <button
-              className={`p-2 px-8 border rounded-md  bg-blue-400 text-white`}
-              onClick={handleConfirm}
-            >
-              {isSubmitSuccess ? "New" : "Confirm"}
-            </button>
-          </div>
-        ) : null}
+        </div>
+        <div className="flex flex-row w-full items-center justify-center ">
+          {isSubmitting ? (
+            <div className="flex flex-row w-full items-center justify-center mt-8">
+              <CircularProgress size="1.5rem" />
+            </div>
+          ) : isSearchingSuccess ? (
+            <div className="flex flex-row w-full items-center justify-center mt-8">
+              <button
+                disabled={isSubmitting}
+                className="p-2 px-8 border rounded-md  bg-slate-400 text-white mr-4"
+              >
+                Cancel
+              </button>
+              <button
+                className={`p-2 px-8 border rounded-md  bg-blue-400 text-white`}
+                onClick={handleConfirm}
+              >
+                {isSubmitSuccess ? "New" : "Confirm"}
+              </button>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
