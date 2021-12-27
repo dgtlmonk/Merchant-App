5507ca5aa952adfa939b7eea5561ab7b1de4f67f
dgtlmonk <dgtlmonk@gmail.com>
2021-12-27 10:29:11 +0800

    feat: icons

--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -10,14 +10,16 @@ export default ({ onMenuSelect }: Props) => (
     <div className="flex flex-row  p-4 flex-wrap w-4/5 justify-around">
       <AppMenuItem
         label="Issue Card"
+        iconCls="picon-barcode"
         onSelect={() => onMenuSelect(VIEWS.ISSUE_CARD)}
       />
       <AppMenuItem
         label="Add Sales"
+        iconCls="picon-order"
         onSelect={() => onMenuSelect(VIEWS.ADD_SALES)}
       />
-      <AppMenuItem label="History" />
-      <AppMenuItem label="Settings" />
+      <AppMenuItem iconCls="picon-clock-outline" label="History" />
+      <AppMenuItem iconCls="picon-setting-outline" label="Settings" />
     </div>
   </div>
 );
--- src/components/AppMenuItem.tsx
+++ src/components/AppMenuItem.tsx
@@ -1,17 +1,20 @@
 type Props = {
   label: string;
+  iconCls: string;
   onSelect?: () => void;
 };
 
-export default ({ label, onSelect }: Props) => {
+export default ({ label, onSelect, iconCls }: Props) => {
   return (
     <div
       style={{ minHeight: "110px", minWidth: "150px" }}
-      className="menu-item border p-4 shadow-sm rounded-md xs:m-2 md:m-8 flex flex-col-reverse items-center"
+      className="menu-item p-4  xs:m-2 md:m-8 flex flex-col-reverse items-center"
       role="button"
       onClick={onSelect}
     >
       <span>{label}</span>
+
+      <span className={`${iconCls} icon-menu`}></span>
     </div>
   );
 };
