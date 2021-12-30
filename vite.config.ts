9961415018d9530455978a22fbd83ca2a449d6c9
dgtlmonk <dgtlmonk@gmail.com>
2021-12-30 10:03:41 +0800

    ci: (vite.config.ts) assets folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,44 @@ import { defineConfig } from "vite";
 const path = require("path");
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  publicDir: "public",
-  plugins: [react()],
+// export default defineConfig(({command, mode}) => {
+// {
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   publicDir: "public/assets",
+//   plugins: [react()],
+// }
+
+// });
+
+export default defineConfig(({ command, mode }) => {
+  if (command === "serve") {
+    return {
+      // dev specific config
+
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      publicDir: "public/assets",
+      plugins: [react()],
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      publicDir: "public",
+      plugins: [react()],
+    };
+  }
 });
